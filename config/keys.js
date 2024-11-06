9598b2b00326304cf9cad9f928575411b0f92e15
Streple Duck <streple@streple>
2024-11-05 20:20:18 -0500

    font

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword22 = 'mongodb://localhost:27017/mern2';
-dbPassword= 'mongodb+srv://BatchFlash:HelloToday12@cluster0.e1sqjnt.mongodb.net/BatchFlashUsers'
+dbPassword= ''
 
 module.exports = {
     mongoURI: dbPassword
